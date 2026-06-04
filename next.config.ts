5914f164c04766d0bf3add7a7c799d53986e0f57
Lynn <lynn@biodlink.com>
2026-06-04 14:38:08 +0800

    fix: add basePath for GitHub Pages subdirectory deployment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const isExport = process.env.NEXT_OUTPUT === "export";
 const nextConfig: NextConfig = {
   trailingSlash: true,
   images: { unoptimized: true },
-  ...(isExport ? { output: "export" } : {}),
+  ...(isExport
+    ? { output: "export", basePath: "/adc-panorama" }
+    : {}),
 };
 
 export default nextConfig;
